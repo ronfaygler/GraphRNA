cbe51f28dade8c7315ca086ae747cbe629bafdfd
ronfaygler <ronfaygler@gmail.com>
2024-10-23 12:02:02 +0300

    the train-test work on dir 0 without dups

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ def main():
     train_test=True
 
     # Number of directories (from 0 to 19)
-    num_folders = 7
+    num_folders = 1
 
     # Function to get the file that starts with 'NPS_CLIP_Random' from a directory
     def get_random_clip_file(dir_path, prefix="NPS_CLIP_Random"):
@@ -88,7 +88,7 @@ def main():
     cv_predictions_dfs = [] # To collect CV predictions
 
     # Loop through 20 folders for both train and test
-    for i in range(1, num_folders):
+    for i in range(num_folders):
         # Construct paths to the current train and test directories
         train_dir = os.path.join(train_root, str(i))
         test_dir = os.path.join(test_root, str(i))
@@ -118,17 +118,14 @@ def main():
         else:
             print(f"Loop {i}: Train or Test file not found.")
 
-        cv_n_splits = 1
-    
         test_predictions_df = train_and_evaluate(model_h=graph_rna, train_fragments=train_fragments, test=test, model_name=model_name , data=data, train_test=train_test, **kwargs)
         cv_predictions_dfs.append(test_predictions_df)
-        
-        # break
+        test_predictions_df.to_csv(join(data_path, f"train_test_predictions/NPS_CLIP_Random/part{i}.csv"), index=False)
 
-    all_folds_predictions = pd.concat(cv_predictions_dfs).reset_index(drop=True)
-    all_folds_predictions.to_csv(join(data_path, "all_folds_predictions.csv"), index=False)
+    # all_folds_predictions = pd.concat(cv_predictions_dfs).reset_index(drop=True)
+    # all_folds_predictions.to_csv(join(data_path, "all_folds_predictions.csv"), index=False)
 
-    return cv_predictions_dfs, all_folds_predictions
+    return
 
         # cv_predictions_dfs, cv_training_history = \
         #     model_h.run_cross_validation(X=train_fragments['X'], y=train_fragments['y'], 
